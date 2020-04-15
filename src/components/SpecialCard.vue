e24e1d05740747d95fb81e6e3e3586f6f4f5910c
Fidel_Yiu <1302139012@qq.com>
2020-04-15 21:27:55 +0800

    卡片阴影

--- src/components/SpecialCard.vue
+++ src/components/SpecialCard.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>Hello</div>
+  <div class="card-body">
+    <img style="width: 100%" src="../assets/image/specialCard/sword.png" alt="">
+  </div>
 </template>
 
 <script>
@@ -9,5 +11,16 @@
 </script>
 
 <style scoped>
+  .card-body {
+    border: 1px solid #e8eaec;
+    background: #fff;
+    border-radius: 4px;
+    font-size: 14px;
+    position: relative;
+    transition: all .5s ease-in-out;
+  }
 
+  .card-body:hover {
+    box-shadow: 0 0 10px #888888;
+  }
 </style>
