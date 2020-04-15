59422e21ab8f7418557d94ac45bef21742a67828
Fidel_Yiu <1302139012@qq.com>
2020-04-15 17:34:58 +0800

    首页顶部图片

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="home">
-    <div style="position: relative">
-      <img style="width: 100%;" src="../../assets/image/home/nav-background.jpg" alt="首页图片">
-      <div style="height: 100%;width: 100%;opacity: .3;background: black;position: absolute;top: 0;left: 0"></div>
+    <div class="top-image-box">
+      <img class="top-image" src="../../assets/image/home/nav-background.jpg" alt="首页图片">
+      <div class="top-image-mask"></div>
     </div>
     <breadcrumb-route></breadcrumb-route>
     <empty-div title="首页"></empty-div>
@@ -21,3 +21,26 @@
     }
   }
 </script>
+
+<style scoped>
+  .top-image-box {
+    position: relative;
+    overflow: hidden;
+  }
+
+  .top-image {
+    width: 100%;
+    height: auto;
+    margin-bottom: -5px;
+  }
+
+  .top-image-mask {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.5);
+    opacity: 0.9;
+  }
+</style>
