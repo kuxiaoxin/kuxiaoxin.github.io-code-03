0f94c921017aa430e8c3c23dcc4d1108c0077147
Fidel_Yiu <1302139012@qq.com>
2020-04-15 17:14:18 +0800

    导航栏完成

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <template v-for="(item, index) in routeList">
-        <router-link :to="item.path" :key="index">{{item.name}}</router-link>
-      </template>
+    <div class="nav-box">
+      <img src="./assets/image/logo/logo.png" class="nav-image" alt="">
+      <div class="nav-title">三国趣谈</div>
+      <div class="nav-deps">从三国中寻求出为人处事之道!</div>
+      <div id="nav" v-for="(item, index) in routeList" :key="index" class="nav-item">
+        <router-link :to="item.path">{{item.name}}</router-link>
+      </div>
     </div>
-    <router-view/>
+    <div style="width: 1080px;margin: 0 auto">
+      <router-view/>
+    </div>
   </div>
 </template>
 
@@ -45,14 +50,52 @@
   }
 </script>
 
-<style>
+<style scoped>
+  .nav-box {
+    width: 1080px;
+    margin: 0 auto;
+    height: 50px;
+    padding: 5px 0;
+    display: flex;
+    line-height: 40px;
+    font-size: 16px;
+  }
+
+  .nav-image {
+    flex-grow: 0;
+    flex-shrink: 0;
+    height: 40px;
+    width: 40px;
+    margin-right: 10px;
+  }
+
+  .nav-title {
+    flex-grow: 0;
+    flex-shrink: 0;
+    font-size: larger;
+    font-weight: bold;
+    margin-right: 10px;
+  }
+
+  .nav-deps {
+    flex-grow: 1;
+    flex-shrink: 0;
+    font-size: smaller;
+    color: #808695;
+  }
+
+  .nav-item {
+    padding: 0 5px;
+    flex-grow: 0;
+    flex-shrink: 0;
+  }
 
   #nav a {
-    color: #ccc;
+    color: #808695;
     text-align: center;
   }
 
   #nav a.router-link-exact-active {
-    color: #42b983;
+    color: #d91419;
   }
 </style>
