1eebc302b4ef6e961e9c887fe77afd191f470e70
Fidel_Yiu <1302139012@qq.com>
2020-04-15 21:46:28 +0800

    卡片阴影

--- src/components/SpecialCard.vue
+++ src/components/SpecialCard.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="card-body">
-    <img style="width: 100%" src="../assets/image/specialCard/sword.png" alt="">
+    <!--<img style="width: 256px;height: 256px;" src="../assets/image/specialCard/sword.png" alt="">-->
+    <img class="card-body-image" src="http://www.lishichunqiu.com/zt/sgssmz/sgssmz.jpg"
+         alt="">
+    <div style="padding: 16px;border-top: 1px solid #e8eaec;">
+      <div class="card-text">历数三国时期的少数民族</div>
+    </div>
   </div>
 </template>
 
@@ -20,7 +25,24 @@
     transition: all .5s ease-in-out;
   }
 
+  .card-body-image {
+    border-top-left-radius: 4px;
+    border-top-right-radius: 4px;
+    width: 256px;
+    height: 256px;
+    margin-bottom: -5px;
+  }
+
   .card-body:hover {
     box-shadow: 0 0 10px #888888;
+  }
+
+  .card-text {
+    font-weight: bold;
+    width: 100%;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 3;
+    overflow: hidden;
   }
 </style>
