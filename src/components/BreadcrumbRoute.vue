dd27d004a6c218db432d7e28ddfe5bf758f66c5f
Fidel_Yiu <1302139012@qq.com>
2020-04-17 11:16:27 +0800

    面包屑修改

--- src/components/BreadcrumbRoute.vue
+++ src/components/BreadcrumbRoute.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="display: flex;justify-content: flex-start;">
+  <div style="display: flex;justify-content: flex-start;padding: 4px;">
     <span style="flex-grow: 0;flex-shrink: 0;color: #808695;">当前位置：</span>
     <Breadcrumb style="flex-grow: 1;flex-shrink: 0;">
       <template v-for="(item, index) in this.$route.meta.breadcrumbItems">
