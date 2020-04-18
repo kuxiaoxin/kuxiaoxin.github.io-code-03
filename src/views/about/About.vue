eec3c62d0ce550ef1d7fa6719c03419f6f5e20ae
Fidel_Yiu <1302139012@qq.com>
2020-04-18 18:53:40 +0800

    关于页面

--- src/views/about/About.vue
+++ src/views/about/About.vue
@@ -1,19 +1,32 @@
 <template>
   <div class="about">
     <breadcrumb-route></breadcrumb-route>
-    <empty-div title="关于"></empty-div>
+    <div style="width: 1200px;position: relative">
+      <img style="width: 100%;margin-bottom: -5px;" src="../../assets/image/about/about-top.jpeg" alt="">
+      <div class="top-image-mask"></div>
+    </div>
   </div>
 </template>
 
 <script>
   import BreadcrumbRoute from "@/components/BreadcrumbRoute"
-  import EmptyDiv from "@/components/EmptyDiv"
 
   export default {
     name: 'About',
     components: {
-      BreadcrumbRoute,
-      EmptyDiv
+      BreadcrumbRoute
     }
   }
 </script>
+
+<style>
+  .top-image-mask {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.5);
+    opacity: 0.9;
+  }
+</style>
